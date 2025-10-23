c9f75d28365c70361519badf8eb25f1ce1df376b
Heck <97088365+0zero4u@users.noreply.github.com>
2025-10-23 19:32:50 +0530

    Update pipeline_registry.py

--- src/binance_pipeline/pipeline_registry.py
+++ src/binance_pipeline/pipeline_registry.py
@@ -1,7 +1,7 @@
-from typing import Dict
+def typing import Dict
 from kedro.pipeline import Pipeline
 
-    def register_pipelines() -> Dict[str, Pipeline]:
+def register_pipelines() -> Dict[str, Pipeline]:
         """Register the project's pipelines.
         Returns:
             A mapping from pipeline names to ``Pipeline`` objects.
