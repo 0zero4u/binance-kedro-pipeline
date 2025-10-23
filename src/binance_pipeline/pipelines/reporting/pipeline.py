5243373248a4a81d31bbbf3bbfc6a1b0737bef0c
Heck <97088365+0zero4u@users.noreply.github.com>
2025-10-23 13:40:32 +0530

    Create pipeline.py

---
+++ src/binance_pipeline/pipelines/reporting/pipeline.py
@@ -0,0 +1,17 @@
+from kedro.pipeline import Pipeline, node
+from .nodes import create_unified_traceability_report
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return Pipeline([
+        node(
+            func=create_unified_traceability_report,
+            inputs=[
+                "merged_tick_data",
+                "resampled_data_100ms",
+                "features_data",
+            ],
+            outputs="unified_5min_traceability_report",
+            name="create_traceability_report_node",
+        )
+    ])
+  
