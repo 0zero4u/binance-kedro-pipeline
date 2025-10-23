df9a2c82486127436761d1627767e18f9e172a58
Heck <97088365+0zero4u@users.noreply.github.com>
2025-10-23 13:34:35 +0530

    Update settings.py

--- src/binance_pipeline/settings.py
+++ src/binance_pipeline/settings.py
@@ -2,6 +2,7 @@ from kedro.pipeline import pipeline
 from binance_pipeline.pipeline import create_pipeline as create_data_engineering_pipeline
 from binance_pipeline.pipelines.lgbm.pipeline import create_pipeline as create_lgbm_pipeline
 from binance_pipeline.pipelines.arf.pipeline import create_pipeline as create_arf_pipeline
+from binance_pipeline.pipelines.reporting.pipeline import create_pipeline as create_reporting_pipeline
 
 # Class that manages Kedro's library components.
 # from kedro.framework.context import KedroContext
@@ -20,7 +21,7 @@ PIPELINES = {
     # The pipeline that runs when you just type "kedro run"
     "__default__": create_data_engineering_pipeline(),
 
-    # The data engineering pipeline, which you can also run by name
+    # The data engineering pipeline, now with validation built-in
     "de": create_data_engineering_pipeline(),
 
     # The original modeling pipeline
@@ -29,7 +30,13 @@ PIPELINES = {
     # The new ARF modeling pipeline
     "arf": create_arf_pipeline(),
     
+    # The new reporting pipeline
+    "reporting": create_reporting_pipeline(),
+    
     # New end-to-end pipelines for convenience
     "e2e_lgbm": pipeline(create_data_engineering_pipeline()) + pipeline(create_lgbm_pipeline()),
     "e2e_arf": pipeline(create_data_engineering_pipeline()) + pipeline(create_arf_pipeline()),
+    
+    # The new end-to-end pipeline with the traceability report
+    "e2e_reporting": pipeline(create_data_engineering_pipeline()) + pipeline(create_reporting_pipeline()),
 }
