afcccd315f43467fb5d02c252c928869da0c0446
Heck <97088365+0zero4u@users.noreply.github.com>
2025-10-23 11:17:53 +0530

    Update pipeline.py

--- src/binance_pipeline/pipelines/lgbm/pipeline.py
+++ src/binance_pipeline/pipelines/lgbm/pipeline.py
@@ -2,21 +2,18 @@ from kedro.pipeline import Pipeline, node
 from .nodes import generate_triple_barrier_labels, train_lgbm_model
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return Pipeline(
-        [
-            node(
-                func=generate_triple_barrier_labels,
-                # CORRECTED PATH: No extra "lgbm."
-                inputs=["features_data", "params:labeling_params"],
-                outputs="labeled_data",
-                name="generate_labels_node",
-            ),
-            node(
-                func=train_lgbm_model,
-                # CORRECTED PATH: No extra "lgbm."
-                inputs=["labeled_data", "params:lgbm_params"],
-                outputs="lgbm_model",
-                name="train_lgbm_node",
-            ),
-        ]
-    )
+    return Pipeline([
+        node(
+            func=generate_triple_barrier_labels,
+            inputs=["features_data", "params:labeling_params"],
+            outputs="labeled_data",
+            name="generate_lgbm_labels_node",
+        ),
+        node(
+            func=train_lgbm_model,
+            inputs=["labeled_data", "params:lgbm_params"],
+            outputs=["lgbm_model", "lgbm_eval_results"],
+            name="train_lgbm_node",
+        ),
+    ])
+    
