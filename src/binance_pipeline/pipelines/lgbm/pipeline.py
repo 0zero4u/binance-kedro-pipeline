9549a4d460b19ec4e47a1f263a2266a7cdd9862f
Heck <97088365+0zero4u@users.noreply.github.com>
2025-10-26 14:06:33 +0530

    Update pipeline.py

--- src/binance_pipeline/pipelines/lgbm/pipeline.py
+++ src/binance_pipeline/pipelines/lgbm/pipeline.py
@@ -1,12 +1,22 @@
-from kedro.pipeline import Pipeline, node
+from kedro.pipeline import Pipeline, node, pipeline
 from .nodes import train_lgbm_model
-from binance_pipeline.pipelines.data_science.nodes import generate_triple_barrier_labels
+from binance_pipeline.pipelines.data_science.nodes import (
+    generate_triple_barrier_labels,
+    split_data # <-- Import the new function
+)
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return Pipeline([
+    return pipeline([
+        # Add the new data splitting node
+        node(
+            func=split_data,
+            inputs="features_data",
+            outputs=["train_features", "test_features"], # You will need to add these to catalog.yml
+            name="split_train_test_data_node",
+        ),
         node(
             func=generate_triple_barrier_labels,
-            inputs=["features_data", "params:labeling_params"],
+            inputs=["train_features", "params:labeling_params"], # Use train_features
             outputs="labeled_data",
             name="generate_lgbm_labels_node",
         ),
@@ -16,4 +26,5 @@ def create_pipeline(**kwargs) -> Pipeline:
             outputs=["lgbm_model", "lgbm_eval_results"],
             name="train_lgbm_with_optuna_node",
         ),
+        # You can add a new node here to evaluate the model on 'test_features'
     ])
