884ae42e4441cbab5332d96b42125f4a1e74d6bc
Heck <97088365+0zero4u@users.noreply.github.com>
2025-10-30 11:24:54 +0530

    Update pipeline.py

--- src/binance_pipeline/pipelines/reporting/pipeline.py
+++ src/binance_pipeline/pipelines/reporting/pipeline.py
@@ -8,8 +8,8 @@ def create_pipeline(**kwargs) -> Pipeline:
             inputs=[
                 "merged_grid_15ms",
                 "primary_features_grid",
-                "ewma_features_grid",
-                "features_data_unvalidated", # <-- FIX: Use the unvalidated data
+                "intelligent_multi_scale_features", # <-- FIX: Use the new dataset
+                "features_data_unvalidated", 
             ],
             outputs="unified_5min_traceability_report",
             name="create_traceability_report_node",
